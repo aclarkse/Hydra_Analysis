024dfbe60b8dd7747b8dd49a76fd4a863716ff25
andreaoclarksevilla <andrea.c.sev@gmail.com>
2021-09-22 21:50:18 -0400

    tiff stack  processing

--- process_tiffs.py
+++ process_tiffs.py
@@ -6,31 +6,34 @@ import skimage.io as skio
 import numpy as np
 import tqdm as tqdm
 
-def process_stack(filename, step_size, new_dir):
+def process_stack(filename, step_size):
     """ Processes a stack of tiff files by extracting slices 
         by a specified step size and saves them as NIfTI files.
 
         Arguments:
             - filename [string]: the filename of the tiff stack to process
             - step_size [int]: the desired increment with which to extract tiff slices
-            - new_dir [string]: the name of the directory in which to save slices
     """
 
     tiff_stack = skio.imread(filename, plugin="tifffile")
     n_frames = tiff_stack.shape[0]
+    h = tiff_stack.shape[1]
+    w = tiff_stack.shape[2]
     print(f"Tiff stack loaded. Found {n_frames} images.")
+
+    # compute the number of frames in the reduced array
+    rd_frames = math.floor(n_frames/step_size) + 1
+    tiff_arr = np.zeros((rd_frames, h, w))
+    i = 0
     for frame in tqdm.tqdm(range(n_frames)):
         if frame % step_size == 0:
-            if len(str(frame)) == 2:
-                # for formatting the filename, prepend extra 0s
-                frame_name = "0" + str(frame)
-            elif len(str(frame)) == 1:
-                frame_name = "00"+ str(frame)
-            else:
-                frame_name = frame
-            # convert to NIfTI type
-            img = nib.Nifti1Image(tiff_stack[frame,:,:], affine=np.eye(4))
-            nib.save(img, os.path.join(new_dir, '{}.nii.gz'.format(frame_name)))
+            tiff_arr[i, :, :] = tiff_stack[frame,:,:]
+            i+= 1 
+
+    # convert to NIfTI image
+    img = nib.Nifti1Image(tiff_arr, affine=np.eye(4))
+    fn = filename.split('.')[0]
+    nib.save(img, os.path.join(f'{fn}_sz_{step_size}.nii.gz'))
 
     processed_imgs = math.floor(n_frames/step_size) + 1
     print(f"Finished processing {processed_imgs} images!")
@@ -41,12 +44,5 @@ if __name__ == "__main__":
                         help='The filename of the tiff stack you wish to process.')
     filename = parser.parse_args().filename                   
 
-    # create a new directory for saving slices
-    parent_dir = os.getcwd()
-    new_dir = os.path.join(parent_dir, "{}_slices".format(filename.split('.')[0]))
-    if not os.path.exists(new_dir):
-        os.mkdir(os.path.join(parent_dir, new_dir))
-        print(f"Directory \'{new_dir}/' created")
-
-    process_stack(filename, 20, new_dir)
+    process_stack(filename, 5)
 
