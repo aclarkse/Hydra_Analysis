7b3b2530959772e396d2914f1f10f8f7cb76ca7a
andreaoclarksevilla <andrea.c.sev@gmail.com>
2021-09-29 18:05:06 -0400

    updated patch generation script

--- make_patches.py
+++ make_patches.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import sys
 import glob
 import tifffile as tiff
@@ -6,41 +7,44 @@ from tqdm import tqdm
 from patchify import patchify
 
 
-def make_patches(image_path, mask_path):
-  imgs = glob.glob(os.path.join(image_path, '*.tif'))
-  mks = glob.glob(os.path.join(mask_path, '*.tif'))
+def make_patches(image_path, mask_path, patch_size):
+  img_stack = tiff.imread(image_path)
+  msk_stack = tiff.imread(mask_path)
   sys.stdout.flush()
 
   print("Processing images...")
   sys.stdout.flush()
-  for img in tqdm(imgs, total=len(imgs)):
-    large = tiff.imread(img)
-    img_patches = patchify(large, (256, 256), step=256)
+  num_images = img_stack.shape[0]
+  for img in tqdm(range(num_images)):
+    large_img = img_stack[img]
+    img_patches = patchify(large_img, (patch_size, patch_size), step=patch_size)
     
     for i in range(img_patches.shape[0]):
       for j in range(img_patches.shape[1]):
         single_patch = img_patches[i,j,:,:]
         imgs_dir = 'patches/images'
-        os.makedirs(imgs_dir, exist_ok = True)
+        os.makedirs(imgs_dir, exist_ok=True)
         tiff.imwrite(os.path.join(imgs_dir, f'patch_{i}_{j}.tif'), single_patch)
               
   print('Processing masks...')
   sys.stdout.flush()
-  for msk in tqdm(mks, total=len(mks)):
-      large = tiff.imread(msk)
-      msk_patches = patchify(large, (256, 256), step=256)
+  num_masks = msk_stack.shape[0]
+  for msk in tqdm(range(num_masks)):
+      large_msk = msk_stack[msk]
+      msk_patches = patchify(large_msk, (patch_size, patch_size), step=patch_size)
       
       for i in range(msk_patches.shape[0]):
         for j in range(msk_patches.shape[1]):
           single_patch = msk_patches[i,j,:,:]
           single_patch = single_patch / 255.
           mks_dir = 'patches/masks'
-          os.makedirs(mks_dir, exist_ok = True)
+          os.makedirs(mks_dir, exist_ok=True)
           tiff.imwrite(os.path.join(mks_dir, f'patch_{i}_{j}.tif'), single_patch)
           
           
 if __name__ == "__main__":
-  image_path = '/Users/clarkao1/Desktop/train/images'
-  mask_path = '/Users/clarkao1/Desktop/train/masks'
+  image_path = '/Users/clarkao1/Desktop/Hydra_Analysis/test/images/test_images_stack.tif'
+  mask_path = '/Users/clarkao1/Desktop/Hydra_Analysis/test/masks/test_mask_stack.tif'
+  patch_size = 256
   
-  make_patches(image_path, mask_path)
+  make_patches(image_path, mask_path, patch_size)
